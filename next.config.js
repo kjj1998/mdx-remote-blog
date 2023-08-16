cd1503b028630b490255066b098156295731f760
Koh Jun Jie <koh.jun.jie@outlook.com>
2023-08-16 11:22:03 +0800

    initial commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/kjj1998/test-blogposts/master/images/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
